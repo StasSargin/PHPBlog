4a4a20eac5fd38b31e67c83055f9487d861a75a1
Sargin Stas <stassargin@gmail.com>
2019-07-30 19:34:25 +0300

    закончил написание блога

---
+++ application/views/main/about.php
@@ -0,0 +1,19 @@
+<header class="masthead" style="background-image: url('/public/images/about-bg.jpg')">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <div class="page-heading">
+                    <h1>Я  Junior PHP Developer</h1>
+                    <span class="subheading">Ищу работу</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+            <p>Заполню позднее</p>
+        </div>
+    </div>
+</div>
